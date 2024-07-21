501987264b38b2b82ae3e2c472f5891763b4aa52
Eneo <kajoeneo2001@gmail.com>
2024-07-21 13:55:18 +0200

    SDA Projects

---
+++ cs/script.js
@@ -0,0 +1,112 @@
+// // /*function sum1(){
+// //     num1 = parseFloat(document.getElementById('num1').value)
+// //     num2 = parseFloat(document.getElementById('num2').value)
+// //     num3 = parseFloat(document.getElementById('num3').value)
+// //     sum = num1+num2+num3
+// //     document.getElementById('sum').innerHTML = sum;
+
+// // }
+
+// // function sum(){
+// //     num1 = parseFloat(document.getElementById('num1').value)
+// //     num2 = parseFloat(document.getElementById('num2').value)
+// //     num3 = parseFloat(document.getElementById('num3').value)
+// //     sum = num1+num2+num3
+// //     console.log(sum)
+// // }*/
+
+// // /*
+// // for(i=10;i<=20;i++){
+// //     console.log("The number is "+i)
+
+// // }*/
+
+// // /*
+// // i=9;
+// // while (i<20){
+// //     i = i + 1;
+// //     console.log("The number is "+i)
+
+// // }
+// // /*
+// // i=9;
+// // do {
+// //     i = i +1;
+// //     console.log("The number is "+i)
+
+// // }while(i<20)
+// // */
+// // /*
+// // for(var i=0;i<=20;i+=2){
+// //     console.log(i)
+// // }*/
+
+
+// // //function nums(a, b){
+// // //    var c;
+// // //    if(a<b){
+// // //
+// // //    }
+// // //    if(a>b){
+// // //        console.log('Change numbers')
+// // //
+// // //    }else{
+// // //    for(var i=a;i<=b;i+=2){
+// // //        console.log(i);
+// // //    }
+// // //}
+// // //}
+// // //nums(300,310)
+
+
+// // //const nums =[1,2,3,4,5,6,7,8,9,10]
+// // //console.log(nums)
+// // //nums.pop()
+// // //console.log(nums)
+// // //nums.push(11)
+// // //console.log(nums)
+// // //nums[1]=0
+// // //console.log(nums)
+// // //for (i=0;i<nums.length;i++){
+// // //    if((i+1)%3===0){
+// // //        nums[i]=0;
+        
+// // //    }
+// // //}
+// // //console.log(nums)
+
+// // //function func2(a,b){
+// // //    var x=1;
+// // //    var result=1;
+// // //    while(x<=b){
+//        result *=a;
+//        x++;
+//    }
+//    return result;
+//}
+//console.log(func2(5,2));
+
+// function func(a, b, c){
+//     for(i=a;i<=b;i++){
+//         if ((i%c)==0){
+//            console.log(i);
+            
+//         }
+
+//     }
+// }
+
+// func(0, 100, 3);
+
+// function multiplicationTable(n){
+//     var line = "";
+//     for(var i=1; i<=4; i++){
+//         for (var j=1; j<=4;j++){
+//             line +=(j *i)+" ";
+//         }
+//         console.log(line);
+//         line =""
+//     }
+// }
+
+// multiplicationTable(4);
